672e4aec2bbf920db71b0e72d29248888b115239
kedmenecr <kedmenec.roberto@gmail.com>
2019-01-12 15:35:34 +0100

    Add global article test for db.

--- test/acceptance_tests/articles.test.ts
+++ test/acceptance_tests/articles.test.ts
@@ -1,4 +1,4 @@
-import * as request from "supertest";
+import * as request from "supertest"
 import { app } from "../../src/server"
 
 describe("GET /articles", () => {
@@ -6,8 +6,26 @@ describe("GET /articles", () => {
         request(app)
             .get("/articles")
             .end((err, res) => {
-                expect(res.status).toBe(200);
-                done();
-            });
-    });
-});
+                expect(res.status).toBe(200)
+                done()
+            })
+    })
+
+    it("SHOULD RETURN 2 aritcle when db has 2 arcilces", done => {
+        const articles = [
+            new Articles("ES6 the starter", 3),
+            new Articles("ES6 the pro", 4)
+        ];
+        await db.createArticle(articles[0])
+        await db.createArticle(articles[1])
+
+        request(app)
+            .get("/articles")
+            .end((err, res) => {
+                expect(res.body).toEqual(articles)
+            })
+    })
+
+
+
+})
